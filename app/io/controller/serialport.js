a433b9400479085b5d00303aaebb0d977cd0c055
plin66 <pglin66@126.com>
2022-03-25 17:30:44 +0800

    修改

--- app/io/controller/serialport.js
+++ app/io/controller/serialport.js
@@ -6,26 +6,109 @@
  * @FilePath: \mps_merchante:\iot\egg_modebus\app\io\controller\serialport.js
  * @Description: 
  */
+//var iconv = require('iconv-lite');  //引入数据编码格式转换模块
 'use strict';
-var serialport;
+var port;
 const Controller = require('egg').Controller;
 const { SerialPort ,SerialPortMock } = require('serialport')
 class DefaultController extends Controller {
+
   async list() {
     const { ctx, app } = this;
     const message = ctx.args[0];
     await ctx.socket.emit('list', await SerialPort.list());
   }
   async connect(){
-    serialport = new SerialPort({ path: 'COM3', baudRate: 9600 })
+    const { ctx, app } = this;
+    const message = ctx.args[0];
+    console.log(message)
+   
+    port = new SerialPort(message)//{ path: 'COM3', baudRate: 9600 }
+    /**
+     * 
+     *   baudrate: baudrate,  //波特率设置
+            databits: databits,  //数据位
+            parity: parity,  //校验位
+            stopbits: stopbits //停止位
+     */
+    port.on('readable', function () {
+      console.log('Data:', port.read())
+    })
+    
+    // Switches the port into "flowing mode"
+    port.on('data', function (data) {
+      console.log('Data:', data)
+    })
+    // autoOpen: false,
+    // port.open(function (err) {
+    //   if (err) {
+    //     return console.log('Error opening port: ', err.message)
+    //   }
+    
+    //   // Because there's no callback to write, write errors will be emitted on the port:
+    //   port.write('main screen turn on')
+    // })
   }
   async write(){
-    serialport.write('ROBOT POWER ON')
+    
+
+    const { ctx, app } = this;
+    const message = ctx.args[0];
+    console.log(message)
+    //var buf_once = new Buffer(message, 'hex');//ascii  utf8 utf16le - 2 或 4 个字节 ucs2 - utf16le  base64  latin1  binary - latin1 hex 
+    port.write(message, function (err, results) {
+      if (err) {
+          console.log('err ' + err);
+      }
+      else {
+       
+          console.log('发送数据字节长度： ' + results);  //发出去的数据字节长度
+      }
+  })
   }
 }
 module.exports = DefaultController;
-// or async functions
-/* exports.ping = async function() {
-  const message = this.args[0];
-  await this.socket.emit('res', `666Hi! I've got your message: ${message}`);
-}; */
+
+/**
+ * 
+ *      import io from 'socket.io-client'
+      let socket = io("ws://localhost:7001", {
+         transports: ['websocket']
+        })
+
+// /不间断尝试重连接
+    socket.on('reconnect_attempt',()=> { 
+      console.log("reconnect")
+      socket.transports = ['websocket','polling', 'flashsocket']; 
+    });
+
+// 重连接时出错
+    socket.on('reconnect_error',(attemptNumber)=> { 
+      console.log(attemptNumber)
+    });
+
+//连接成功走这个方法
+    socket.on('connect',()=>{
+      console.log(socket.connected)
+    })
+
+//报错时走这个方法
+    socket.on('connect_error', (error) => {
+      console.log(error)
+    });
+
+//连接存活验证 
+    socket.on('ping', (error) => {
+      console.log('ping_include')
+    });
+
+    socket.emit('serialport/list','');
+    socket.on('list', (res) => {
+      console.log(res)
+    });
+    //socket.emit('serialport/connect','');
+    socket.emit('serialport/write','');
+
+
+ * 
+ */
---
+++ ws.vue
@@ -0,0 +1,100 @@
+<!--
+ * @Author: pglin66@126.com
+ * @Date: 2022-03-25 11:50:31
+ * @LastEditors: pglin66@126.com
+ * @LastEditTime: 2022-03-25 17:21:27
+ * @FilePath: \mps_merchant\src\views\shops\pushShop\ws.vue
+ * @Description: 
+-->
+<template>
+    <div>
+        <el-select placeholder="" v-model="form.path">
+            <el-option :label="item.path" v-for="item in COMList" :value="item.path"></el-option>
+        </el-select>
+        <el-select placeholder="数据位" v-model="form.databits">
+            <el-option :label="7" :value="7"></el-option>
+            <el-option :label="8" :value="8"></el-option>
+        </el-select>
+        <el-select placeholder="波特率" v-model="form.baudRate">
+            <el-option :label="item"
+                v-for="item in [4800,9600,19200,38400,57600,78400,115200,230400,460800,576000]"
+                :value="item"></el-option>
+        </el-select>
+        <el-select placeholder="校验位" v-model="form.parity">
+            <el-option label="None" value="None"></el-option>
+            <el-option label="Even" value="Even"></el-option>
+            <el-option label="Mark" value="Mark"></el-option>
+            <el-option label="Odd" value="Odd"></el-option>
+            <el-option label="Space" value="Space"></el-option>
+        </el-select>
+        <el-select placeholder="停止位" v-model="form.stopbits">
+            <el-option label="1" value="1"></el-option>
+            <el-option label="2" value="2"></el-option>
+        </el-select>
+        <el-button @click="hanbleContent">1</el-button>
+        <el-button @click="hanbleWrite">2</el-button>
+    </div>
+
+</template>
+
+<script setup>
+    import io from 'socket.io-client'
+
+    const state = reactive({
+        COMList: [],
+       
+        form:{
+            path:'COM3',
+            baudRate: 9600,  //波特率设置
+            databits: 7,  //数据位
+            parity: 'None',  //校验位
+            stopbits: 1 //停止位
+        }
+    })
+
+    let socket = io("ws://localhost:7001", {
+        transports: ['websocket']
+    })
+
+    // /不间断尝试重连接
+    socket.on('reconnect_attempt', () => {
+        console.log("reconnect")
+        socket.transports = ['websocket', 'polling', 'flashsocket'];
+    });
+
+    // 重连接时出错
+    socket.on('reconnect_error', (attemptNumber) => {
+        console.log(attemptNumber)
+    });
+
+    //连接成功走这个方法
+    socket.on('connect', () => {
+        console.log(socket.connected)
+    })
+
+    //报错时走这个方法
+    socket.on('connect_error', (error) => {
+        console.log(error)
+    });
+
+    //连接存活验证 
+    socket.on('ping', (error) => {
+        console.log('ping_include')
+    });
+
+    socket.emit('serialport/list', '');
+    socket.on('list', (res) => {
+        state.COMList = res
+        console.log(res)
+    });
+    //socket.emit('serialport/connect','');
+    //socket.emit('serialport/write', '');
+    const hanbleContent = () => {
+        socket.emit('serialport/connect', state.form);
+    }
+
+    const hanbleWrite = () => {
+        socket.emit('serialport/write', '1');
+    }
+    const { COMList,form } = toRefs(state)
+</script>
